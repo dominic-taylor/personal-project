96ee2904156ee440f242a36c80a33dcf200964d4
Dominic Taylor <dataylorx@gmail.com>
2016-03-19 15:21:26 +1300

    Add addImage func to test and bundle.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("welcome to ComiCapp")
+module.exports = {
+  addImage: function () {
+    var img = '/test/images/100x100-smile.png'
+
+    var panel = document.getElementById('panel')
+    var ctx = panel.getContext('2d')
+
+    ctx.drawImage(img)
+  }
+
+}
