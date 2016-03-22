cb3f972a54cd955988488113273b90a91b952688
Dominic Taylor <email": null>
2016-03-23 12:25:22 +1300

    Add addImage function to add image to canvas

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-module.exports = {
-  addImage: function () {
-    var img = '/test/images/100x100-smile.png'
-
+// module.exports = {
+  function addImage() {
     var panel = document.getElementById('panel')
     var ctx = panel.getContext('2d')
+    console.log(ctx.drawImage)
+    var img = document.createElement("img")
 
-    ctx.drawImage(img)
+    img.id = 'image'
+    img.src = './images/smiley.png'
+    console.log(img)
+    ctx.drawImage(img, 0, 0, 100, 100)
+    console.log("add img button click avtivating function")
   }
 
-}
+// }
