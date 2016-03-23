eaa6544117355ec9b67bceba94c88a8c82895503
Dominic Taylor <email": null>
2016-03-23 19:26:03 +1300

    Add unselect after image posted

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
     console.log(el.id, 'has been clicked.')
     console.log(el.className)
     el.className = el.className+' panel-active'
-    return (el.id)
-  }
+    }
 
   function addImage() {
     // need to be able to specify image src and element it will be posted to
@@ -12,6 +11,7 @@
     var panel = document.getElementsByClassName('panel-active')
     console.log(panel[0].id)
     document.getElementById(panel[0].id).innerHTML='<img src="./images/smiley.png"/> '
+    document.getElementById(panel[0].id).classList.remove('panel-active')
   }
 
 // }
