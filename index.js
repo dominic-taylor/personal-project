8b6231c204a91d074e19e15b283783d9c7d696a7
Dominic Taylor <email": null>
2016-03-28 19:05:51 +1300

    Add panel resize and panel destroy

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-// $(document).ready(function() {
+$(document).ready(function() {
+ $(".panel").click(markActivePanel)
+ $(".big-panel").click(markActivePanel)
+ $("#menu").resizable()
+ });
+// }
+//LOOK UP SVG
 
-  function markActivePanel(el) {
+function destroyPanel(event) {
+  $('.panel-active').remove()
+}
+
+
+function markActivePanel(event) {
     $('.panel-active').removeClass('panel-active')
-    // console.log(el.id, 'has been clicked.')
-    el.className = el.className+' panel-active'
-    drag()
-    resize()
-  }
+    $(this).addClass("panel-active")
+    $(this).draggable({
+      cursor: "grabbing"
+    })
+
+    $(this).resizable({
+          start: function(event, ui) {
+            $(this).css({
+              position: "relative !important",
+              top: "0 !important",
+              left: "0 !important"
+            });
+          },
+          stop: function(event, ui) {
+            $(this).css({
+              position: "",
+              top: "",
+              left: ""
+            });
+          }
+    })
+}
 
   function addImage() {
     // need to be able to specify image src and element it will be posted to
@@ -17,16 +45,11 @@
     document.getElementById(panel[0].id).classList.remove('panel-active')
   }
 
- function drag() {
-   $('.panel-active').draggable();
-   $('.panel-active').resizable({
-    animate: true
-   });
- }
+ // function drag() {
+ //   $('.panel-active').draggable();
+ //   $('.panel-active').resizable();
+ // }
 
- function resize() {
+ // function resize() {
 
- }
- // });
-// }
-//LOOK UP SVG
+ // }
